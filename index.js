071ac1bd2e237aaced9290216757ee22723ab774
Chad <chadananda@gmail.com>
2014-06-24 08:12:16 -0700

    added package.json, index.js and modified template

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Convert .fodt documents into simple HTML
+ *
+ * @param  {String} html
+ * @return {String}
+ */
+module.exports = {
+  convert: function(source, dest) {
+    source = source | 'fodt/';
+    dest = dest | 'dest/';
+
+
+
+
+
+
+  }
+};
